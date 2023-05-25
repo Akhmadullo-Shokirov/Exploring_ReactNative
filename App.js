5aaceb4daa6350aad4a41e157a0d83a7689b6f63
Akhmadullo Shokirov <akhmadullo2001@outlook.com>
2023-05-25 22:34:21 +0200

    Apply Layouts and Flexbox practice

--- App.js
+++ App.js
@@ -1,22 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your course goal!'/>
-        <Button title='Add Goal' />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainter}>
+        <Text>List of goals..</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer:{
-    padding: 50
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+    justifyContent: 'center'
+  },
+  goalsContainter: {
+    flex: 5
   }
 });
